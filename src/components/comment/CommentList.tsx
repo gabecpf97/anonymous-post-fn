f3cc8cab36786e2e98ecc073a36ac8b7995111b4
Gabriel <gabrielcpf2997@gmail.com>
2022-02-16 19:17:24 -0800

    added comment list

---
+++ src/components/comment/CommentList.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { ErrorDetail, FetchedData } from '../../interfaces/myInterfaces';
+import { Error } from '../general/Errors';
+
+export const CommentList: React.FC = () => {
+    const [comments, setComments] = useState<string[]>();
+    const [errors, setErrors] = useState<ErrorDetail[] | string>();
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                // fetch to get comment list
+                const response: Response = await fetch(`http://localhost:5000/comments`, {
+                    headers: {
+                        "Authorization": `Bearer ${localStorage.getItem('token')}`,
+                    }
+                });
+                const data: FetchedData = await response.json();
+                if (data.err) {
+                    setErrors(data.err);
+                } else {
+                    setComments(data.theComments);
+                }
+            } catch (err: any) {
+                setErrors(err);
+            }
+        }
+        fetchComments();
+    }, []);
+
+    return (
+        <div>
+            {/* Show comments */}
+            {errors && <Error errors={errors} />}
+        </div>
+    )
+}

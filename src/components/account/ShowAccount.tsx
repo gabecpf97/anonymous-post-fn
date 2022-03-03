ec209e2073437e0d675b5f4057509855422db8ba
Gabriel <gabrielcpf2997@gmail.com>
2022-03-02 22:50:28 -0800

    added user info

--- src/components/account/ShowAccount.tsx
+++ src/components/account/ShowAccount.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { UserType } from "../../interfaces/myInterfaces";
 
 export const ShowAccount: React.FC = () => {
+    const curUser: UserType = JSON.parse(localStorage.getItem('user') || '');
 
     return (
         <div>
-            
+            <p>{curUser.username}</p>
+            <p>{curUser.date_join}</p>
         </div>
     )
 }

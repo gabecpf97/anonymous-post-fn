4a9d7524509fed1d9799ee2ef7f75b3e6c96c12c
Gabriel <gabrielcpf2997@gmail.com>
2022-03-08 23:16:20 -0800

    added fetch in show account

--- src/components/account/ShowAccount.tsx
+++ src/components/account/ShowAccount.tsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { ErrorDetail, UserType } from "../../interfaces/myInterfaces";
+import { ErrorDetail, FetchedData, UserType } from "../../interfaces/myInterfaces";
 
 export const ShowAccount: React.FC = () => {
     const curUser: UserType = JSON.parse(localStorage.getItem('user') || '');
     const [posts, setPosts] = useState<string[]>();
     const [liked, setLiked] = useState<string[]>();
     const [comments, setComments] = useState<string[]>();
-    const [errors, setErrors] = useState<ErrorDetail | string>();
+    const [errors, setErrors] = useState<ErrorDetail[] | string>();
 
     useEffect(() => {
         const fetchData: Function = async () => {
             try {
-                
+                const response: Response = await fetch(`http://localhost:5000/user`, {
+                    headers: {
+                        "Authorization": `Bearer ${localStorage.getItem("token")}`
+                    }
+                });
+                const data: FetchedData = await response.json();
+                if (data.err) {
+                    setErrors(data.err);
+                } else {
+                    setPosts(data.thePosts);
+                    // setLiked(data.thePosts);
+                    setComments(data.theComments);
+                }
             } catch (err: any) {
                 setErrors(err);
             }

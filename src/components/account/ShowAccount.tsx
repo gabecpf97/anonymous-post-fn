cea4a487d2017589817e9922a82c00a751689c02
Gabriel <gabrielcpf2997@gmail.com>
2022-03-08 23:11:11 -0800

    added basic state for show account

--- src/components/account/ShowAccount.tsx
+++ src/components/account/ShowAccount.tsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
-import { UserType } from "../../interfaces/myInterfaces";
+import React, { useEffect, useState } from 'react'
+import { ErrorDetail, UserType } from "../../interfaces/myInterfaces";
 
 export const ShowAccount: React.FC = () => {
     const curUser: UserType = JSON.parse(localStorage.getItem('user') || '');
+    const [posts, setPosts] = useState<string[]>();
+    const [liked, setLiked] = useState<string[]>();
+    const [comments, setComments] = useState<string[]>();
+    const [errors, setErrors] = useState<ErrorDetail | string>();
+
+    useEffect(() => {
+        const fetchData: Function = async () => {
+            try {
+                
+            } catch (err: any) {
+                setErrors(err);
+            }
+        };
+        fetchData();
+    }, [curUser]);
 
     return (
         <div>

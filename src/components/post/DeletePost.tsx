521e0df3075de9bd51038637489b8c6fb4707942
Gabriel <gabrielcpf2997@gmail.com>
2022-03-15 15:57:39 -0700

    added ui for delete post

--- src/components/post/DeletePost.tsx
+++ src/components/post/DeletePost.tsx
@@ -1,6 +1,7 @@
 import React, { SyntheticEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ErrorDetail, FetchedData } from '../../interfaces/myInterfaces';
+import { Error } from '../general/Errors';
 
 interface props {
     id: string
@@ -28,7 +29,11 @@ export const DeletePost: React.FC<props> = ({ id }) => {
 
     return (
         <div>
-
+            <form onSubmit={(e: SyntheticEvent) => handleSubmit(e)}>
+                <label>Delete this post?</label>
+                <input type="submit" value="Delete" />
+            </form>
+            {errors && <Error errors={errors} />}
         </div>
     )
 }

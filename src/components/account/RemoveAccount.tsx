471d33761df31bc95a5d5382dced868ff7037b4f
Gabriel <gabrielcpf2997@gmail.com>
2022-03-24 16:10:41 -0700

    added funcitons to remove account

--- src/components/account/RemoveAccount.tsx
+++ src/components/account/RemoveAccount.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FetchedData } from '../../interfaces/myInterfaces';
 
 export const RemoveAccount: React.FC = () => {
+    const [buttStatus, setButStatus] = useState<boolean>();
+
+    const changeStatus = () => {
+        setButStatus(!buttStatus);
+    }
+
+    const handleRemove = async () => {
+        const response: Response = await fetch(`http://locahost:5000/user/remove`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const data: FetchedData = await response.json();
+        if (data.err) {
+            // error handle
+        } else {
+            // log out and go to login / sign up page
+        }
+    }
 
     return (
         <div>
-            
+
         </div>
     )
 }

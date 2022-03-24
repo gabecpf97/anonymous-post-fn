e2c22481c38783a12a58533b8a74c163b187f92b
Gabriel <gabrielcpf2997@gmail.com>
2022-03-24 16:14:14 -0700

    finished ui for remove account

--- src/components/account/RemoveAccount.tsx
+++ src/components/account/RemoveAccount.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { FetchedData } from '../../interfaces/myInterfaces';
 
 export const RemoveAccount: React.FC = () => {
-    const [buttStatus, setButStatus] = useState<boolean>();
+    const [buttStatus, setButStatus] = useState<boolean>(false);
 
     const changeStatus = () => {
         setButStatus(!buttStatus);
@@ -24,7 +24,16 @@ export const RemoveAccount: React.FC = () => {
 
     return (
         <div>
-
+            {buttStatus && 
+                <div>
+                    <h2>Remove account?</h2>
+                    <button onClick={() => changeStatus()}>Cancel</button>
+                    <button onClick={() => handleRemove()}>Yes</button>
+                </div>
+            }
+            {!buttStatus &&
+                <button onClick={() => changeStatus()}>Remove Account</button>
+            }
         </div>
     )
 }

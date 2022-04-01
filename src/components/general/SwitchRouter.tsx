e14474b72f40095d2c84076892ec212fa74e49de
Gabriel <gabrielcpf2997@gmail.com>
2022-04-01 15:44:21 -0700

    added some route to routes

--- src/components/general/SwitchRouter.tsx
+++ src/components/general/SwitchRouter.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { EditAccount } from '../account/EditAccount'
 import { LogIn } from '../account/LogIn'
+import { ShowAccount } from '../account/ShowAccount'
 import { SignUp } from '../account/SignUp'
+import { CreatePost } from '../post/CreatePost'
 import { Home } from './Home'
 import { Nav } from './Nav'
 
@@ -14,6 +17,9 @@ export const SwitchRouter: React.FC = () => {
                 <Route path='/' element={<Home />} />
                 <Route path='/sign-up' element={<SignUp />} />
                 <Route path='/log-in' element={<LogIn />} />
+                <Route path='/account' element={<ShowAccount />} />
+                <Route path='/account/edit' element={<EditAccount />} />
+                <Route path='/create/post' element={<CreatePost />} />
             </Routes>
         </BrowserRouter>
     )

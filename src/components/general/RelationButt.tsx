4b977ea8bfe56a579e9a2f1f75ab14b6dc749efd
Gabriel <gabrielcpf2997@gmail.com>
2022-04-05 16:35:26 -0700

    added relation butt for likes and unlikes

---
+++ src/components/general/RelationButt.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { FetchedData } from '../../interfaces/myInterfaces';
+
+interface props {
+    id: string
+}
+
+export const RelationButt: React.FC<props> = ({ id }) => {
+    const [buttStatus, setButtStatus] = useState<boolean>();
+
+    const handleChagnes = async () => {
+        const response: Response = await fetch(`http://localhost:5000/post/like/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const data: FetchedData = await response.json();
+        if (data.err) {
+            // error handle
+        } else {
+            setButtStatus(!buttStatus);
+        }
+    }
+
+    return (
+        <div>
+            <button onChange={() => handleChagnes()}>
+                {buttStatus? 'unlike' : 'like'}
+            </button>
+        </div>
+    )
+}
